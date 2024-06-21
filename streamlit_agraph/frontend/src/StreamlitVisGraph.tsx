e3cd123c249f944c8a31d21eefffaabcc6dc5833
Chi Shu <macbookpro@MacBookdeMacBook-Pro.local>
2024-06-21 16:52:40 -0400

    update code with functional component and hook

--- streamlit_agraph/frontend/src/StreamlitVisGraph.tsx
+++ streamlit_agraph/frontend/src/StreamlitVisGraph.tsx
@@ -1,70 +1,47 @@
-import {
-  Streamlit,
-  StreamlitComponentBase,
-  withStreamlitConnection,
-} from "streamlit-component-lib"
-import React, { ReactNode } from "react"
-import VisGraph from 'react-vis-graph-wrapper';
+import React from 'react';
+import VisGraph, {GraphData, GraphEvents, Options} from 'react-vis-graph-wrapper';
+import { Streamlit } from "streamlit-component-lib";
+import { useRenderData } from "streamlit-component-lib-react-hooks";
 
+function StreamlitVisGraph() {
+  const renderData = useRenderData();
 
-class StreamlitVisGraph extends StreamlitComponentBase {
+  const graphIn = JSON.parse(renderData.args["data"])
 
-  public render = (): ReactNode => {
+  const options: Options = JSON.parse(renderData.args["config"])
 
-    function lookup_node_id(lookup_node, mynodes){
-      for (let node of mynodes){
-          if (node.id === lookup_node){
-              return node;
-          }
-    }}
+  const lookupNodeId = (lookupNode, myNodes) => myNodes.find(node => node.id === lookupNode);
 
-    var graph = JSON.parse(this.props.args["data"]);
-    
-    var nodes = graph.nodes.slice();
+  const graph: GraphData = {nodes: graphIn.nodes.slice(), edges: graphIn.edges.slice()}
 
-    for (let i = 0; i < nodes.length; i++) {
-      if(nodes[i].title)
-        nodes[i].div = this.htmlTitle(nodes[i].title);
+  const events: GraphEvents = {
+    selectNode: (event) => {
+      Streamlit.setComponentValue(event.nodes[0]);
     }
-  
-    const options = JSON.parse(this.props.args["config"]);
-
-    const events = {
-
-      selectNode: (event:any) => {
-        Streamlit.setComponentValue(event.nodes[0]);
-      },
-
-      doubleClick: (event:any) => {
-        console.log(event.nodes);
-        // let link = nodes;
-        let lookup_node = lookup_node_id(event.nodes[0], nodes);
-        let link = lookup_node.div.innerHTML;
-        if(link){
+    ,
+    doubleClick: (event) => {
+      const lookupNode = lookupNodeId(event.nodes[0], graph.nodes);
+      if (lookupNode && lookupNode.link) {
+        const link = lookupNode.link;
+        if (link) {
           window.open(link);
         }
       }
-    };
-    return (
-      <span>
-    
-      <VisGraph
-      graph={graph}
-      options={options}
-      events={events}
-      getNetwork={(network: any) => {
-        //  if you want access to vis.js network api you can set the state in a parent component using this property
-        //console.log(network);
-      }}/>
-      </span>
-    )
-  }
+    }
+  };
 
-  private htmlTitle = (html):any => {   
-    const container = document.createElement("div");
-    container.innerHTML = html;
-    return container;
-  }
+  return (
+    <span>
+      <VisGraph
+        graph={graph}
+        options={options}
+        events={events}
+        ref = {(network: any) => {
+          // console.log(network)
+        }}
+      />
+    </span>
+  )
 }
 
-export default withStreamlitConnection(StreamlitVisGraph)
+export default StreamlitVisGraph;
